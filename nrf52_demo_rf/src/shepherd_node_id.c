91295ab429bcb21181770025cd1141bc743c32f4
Ingmar Inʒo Splitt <orgua@users.noreply.github.com>
2023-06-12 18:59:54 +0200

    Update shepherd_node_id.c

--- nrf52_demo_rf/src/shepherd_node_id.c
+++ nrf52_demo_rf/src/shepherd_node_id.c
@@ -1,14 +1,12 @@
 
-// SHEPHERD_NODE_ID is a variable with very special handling: on SHEPHERD, its init value
+// SHEPHERD_NODE_ID is a variable with special handling: for the SHEPHERD-testbed, its init value
 // gets overridden with the id of the node in the testbed during device programming (by patching
 // the elf file before programming). Thus, it is well suited as a node id variable.
 //
-// ATTENTION: The patching mechanism is a sensible thing. To work properly, the most important
+// ATTENTION: The patching mechanism is a sensitive thing. To work properly, the most important
 // prerequisite is that SHEPHERD_NODE_ID is placed in a section of type SHT_PROGBITS (see ELF spec.
 // for details), i.e. a section with a 1:1 content image in the elf file (which gets patched then).
-// (In the past it was also required to be the .data section (".data", not ".data.<something>").
-// Starting from 2022-02-21 the patch tool looks for a section containing symbol SHEPHERD_NODE_ID,
-// so today SHEPHERD_NODE_ID can also reside in .rodata or any other section.)
+//
 // In projects using the SEGGER linker, the .data segment(s) are typically not stored explicitly
 // in the elf file (they have type SHT_NOBITS and get initialized implicitly from .segger.init.data
 // via the init table functions). Hence, SHEPHERD_NODE_ID must not reside in .data in this case.

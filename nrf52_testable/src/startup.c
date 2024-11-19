e9d3c81348c521872f9d55156ebc933dc1cabf37
Ingmar Splitt <orgua@users.noreply.github.com>
2024-11-19 12:23:09 +0100

    Update startup.c

--- nrf52_testable/src/startup.c
+++ nrf52_testable/src/startup.c
@@ -162,5 +162,16 @@ void c_startup(void)
     src = &_sbss;
     while (src < &_ebss) *(src++) = 0;
 
+    /* Configure NFCT pins as GPIOs since there is no NFC. */
+    if ((NRF_UICR->NFCPINS & UICR_NFCPINS_PROTECT_Msk) == (UICR_NFCPINS_PROTECT_NFC << UICR_NFCPINS_PROTECT_Pos)){
+            NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Wen << NVMC_CONFIG_WEN_Pos;
+            while (NRF_NVMC->READY == NVMC_READY_READY_Busy){}
+            NRF_UICR->NFCPINS &= ~UICR_NFCPINS_PROTECT_Msk;
+            while (NRF_NVMC->READY == NVMC_READY_READY_Busy){}
+            NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Ren << NVMC_CONFIG_WEN_Pos;
+            while (NRF_NVMC->READY == NVMC_READY_READY_Busy){}
+            NVIC_SystemReset();
+    }
+
     main();
 }
